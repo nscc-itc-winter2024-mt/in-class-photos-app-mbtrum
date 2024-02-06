9b310037940d25b5b67c5da638c4365d66f94ff8
Michael Trumbull <W0304263@campus.nscc.ca>
2024-02-06 16:16:38 -0400

    Front page design.

---
+++ Photos/Pages/DisplayPhoto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Photos.Pages
+{
+    public class DisplayPhotoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Photos/Pages/Index.cshtml.cs
+++ Photos/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Photos.Data;
+using Photos.Models;
 
 namespace Photos.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PhotosContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<Photo> Photos { get; set; } = default!;
+
+        public IndexModel(ILogger<IndexModel> logger, PhotosContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            Photos = await _context.Photo.ToListAsync();
         }
     }
 }
