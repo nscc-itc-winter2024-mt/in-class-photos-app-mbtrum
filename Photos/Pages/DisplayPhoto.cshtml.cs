bbb8d64d2bcc76d49d247627f8c0effc9895bcb3
W0304263 <W0304263@172LAB-81260>
2024-02-07 14:27:38 -0400

    More UI updates.

--- Photos/Pages/DisplayPhoto.cshtml.cs
+++ Photos/Pages/DisplayPhoto.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Photos.Data;
+using Photos.Models;
+
 
 namespace Photos.Pages
 {
     public class DisplayPhotoModel : PageModel
     {
-        public void OnGet()
+        private readonly Photos.Data.PhotosContext _context;
+
+        public Photo Photo { get; set; } = default!;
+
+        // Constructor
+        public DisplayPhotoModel(PhotosContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photo.FirstOrDefaultAsync(m => m.PhotoId == id);
+
+            if (photo == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Photo = photo;
+            }
+            return Page();
         }
     }
 }
