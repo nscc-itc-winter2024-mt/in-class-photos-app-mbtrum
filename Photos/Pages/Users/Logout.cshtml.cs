68763dadf70e0f0f67b9ca0fcc8ebd541f893bab
Michael Trumbull <W0304263@campus.nscc.ca>
2024-02-15 11:21:41 -0400

    Setting up pages for login

---
+++ Photos/Pages/Users/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Photos.Pages.Users
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
